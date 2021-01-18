8079483abacfa461e399b25191576b962f8b644f
Sandipan Roy <76780836+sandiproycse@users.noreply.github.com>
2021-01-18 12:22:52 +0600

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -28,9 +28,9 @@ int main()
 }
 void bubble_sort(int arr[],int size)
 {
-    for(int i = 0;i < size; i ++){
-        for(int j = 0; j < (size - (i + 1)); j++){
-            cout << j << " " << (j + 1) << endl;
+    for(int i = 0;i < size - 1; i ++){
+        for(int j = 0; j < (size - (i+1)); j++){
+            //cout << "arr["<< j << "] > arr[" << (j + 1) << "] " << arr[j] << " > " << arr[j+1] << endl;
 
             if(arr[j] > arr[j+1]){
                 int temp = arr[j];
@@ -38,7 +38,8 @@ void bubble_sort(int arr[],int size)
                 arr[j+1] = temp;
             }
         }
-        cout << endl;
+       // for(int k = 0;k < size; k++) cout << arr[k] << " " ;
+        //cout << endl << endl;
     }
 }
 int binary_search(int arr[],int size, int key)
